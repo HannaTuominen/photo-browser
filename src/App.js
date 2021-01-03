c68eb2cddc59b3edbd3fa81a0292fcc42ceea95b
Hanna Tuominen <hanna.h.tuominen@tuni.fi>
2021-01-03 13:33:04 +0200

    Add lazyloading to photodisplayer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import React from "react";
+import React, {Suspense} from "react";
 
 import PhotoBrowser from "./containers/PhotoBrowser/PhotoBrowser";
 import Layout from "./hoc/Layout/Layout";
 import {Redirect, Route} from "react-router-dom";
-import PhotoDisplayer from "./containers/PhotoDisplayer/PhotoDisplayer";
+import LoadingIndicator from "./components/UI/LoadingIndicator/LoadingIndicator";
+
+const PhotoDisplayer = React.lazy(() => {
+  return import('./containers/PhotoDisplayer/PhotoDisplayer');
+});
 
 const App = props => {
   return (
     <div>
       <Layout>
+        <Suspense fallback={<div style={{marginTop: '100px'}}> <LoadingIndicator height={"100px"}/></div>}>
         <Route exact path="/">
-          <Redirect to="/gallery/album/1" />
+          <Redirect to="/gallery/album/1"/>
         </Route>
-        <Route path="/gallery/album/:albumId" component={PhotoBrowser}/>
-        <Route path="/gallery/album/:albumId/image/:imageId" component={PhotoDisplayer} />
+        <Route path="/gallery/album/:albumId" render={(props) => <PhotoBrowser {...props}/>}/>
+        <Route path="/gallery/album/:albumId/image/:imageId" render={(props) => <PhotoDisplayer {...props}/>} /></Suspense>
       </Layout>
     </div>
   );
