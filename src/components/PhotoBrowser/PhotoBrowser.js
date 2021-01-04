526e538caca8db4c7b8d1bca3bf180cb0a04acdc
Hanna Tuominen <hanna.h.tuominen@tuni.fi>
2021-01-04 08:28:45 +0200

    Add react lazy to thumbnails and photo displayer

--- src/components/PhotoBrowser/PhotoBrowser.js
+++ src/components/PhotoBrowser/PhotoBrowser.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 
 import Auxiliary from "../../hoc/Auxiliary/Auxiliary";
-import ThumbnailsDisplayer from "../../containers/ThumbnailsDisplayer/ThumbnailsDisplayer";
+// import ThumbnailsDisplayer from "../../containers/ThumbnailsDisplayer/ThumbnailsDisplayer";
 import AlbumsDisplayer from "../../containers/AlbumsDisplayer/AlbumsDisplayer";
 import {Route, Switch} from "react-router-dom";
-import PhotoDisplayer from "../../containers/PhotoDisplayer/PhotoDisplayer";
+// import PhotoDisplayer from "../../containers/PhotoDisplayer/PhotoDisplayer";
 
 import './PhotoBrowser.css';
 import AlbumsWelcome from "./AlbumsWelcome/AlbumsWelcome";
+import LoadingIndicator from "../UI/LoadingIndicator/LoadingIndicator";
+
+const ThumbnailsDisplayer = React.lazy(() => {
+  return import('../../containers/ThumbnailsDisplayer/ThumbnailsDisplayer');
+});
+
+const PhotoDisplayer = React.lazy(() => {
+  return import('../../containers/PhotoDisplayer/PhotoDisplayer');
+});
 
 const PhotoBrowser = props => {
 
@@ -15,11 +24,13 @@ const PhotoBrowser = props => {
       <Auxiliary>
           <div>
             <AlbumsDisplayer/>
+            <Suspense fallback={<div style={{marginTop: '100px'}}> <LoadingIndicator height={"100px"}/></div>}>
             <Switch>
               <Route exact path="/gallery/albums" render={(props) => <AlbumsWelcome/>}/>
               <Route path="/gallery/albums/:albumId" render={(props) => <ThumbnailsDisplayer{...props} />}/>
             </Switch>
             <Route path="/gallery/albums/:albumId/image/:imageId" render={(props) => <PhotoDisplayer {...props}/>} />
+            </Suspense>
           </div>
       </Auxiliary>
     );
